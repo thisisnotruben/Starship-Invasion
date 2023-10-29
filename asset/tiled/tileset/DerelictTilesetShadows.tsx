8d251b7de8b88926d21c0e553cd80d95f3ea84f9
Ruben Alvarez Reyes <rubenar1996@gmail.com>
2023-10-29 11:09:37 -0500

    removed tiled folder

--- asset/tiled/tileset/DerelictTilesetShadows.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="DerelictTilesetShadows" tilewidth="8" tileheight="8" tilecount="903" columns="21">
- <image source="../Scifi_SpaceDerelict/Tileset/DerelictTilesetShadows.png" width="168" height="344"/>
-</tileset>
